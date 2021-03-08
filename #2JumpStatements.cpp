450c02711d543e2a5f5218925017575a1e58d824
Patlolla Sravanthi <63411144+patil-sravanthi@users.noreply.github.com>
2021-03-08 23:08:25 +0530

    Create #2JumpStatements.cpp

---
+++ #2JumpStatements.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int N;
+    cin>>N;
+    // YOUR CODE GOES HERE
+   for(int i=0;i<N<<i++){
+       if(i%2==0){
+           continue;
+       }
+       else{
+            cout<<i<<endl;
+        }
+    }
+    
+    return 0;
+}
