14928cf3945171fdf33523a40cf9c7a2466b1acd
Patlolla Sravanthi <63411144+patil-sravanthi@users.noreply.github.com>
2021-03-08 23:03:18 +0530

    Create #Day2-ComparisonOperator_and_If_else.cpp

---
+++ #Day2-ComparisonOperator_and_If_else.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int num;
+    cin>>num;
+    // Your code goes here
+    if(num >= 90){
+    cout<<"grade A."<<endl;}
+    else if(num >= 80)
+     { cout<<"grade B."<<endl;}
+       else if(num >= 70)
+      {cout<<"grade C."<<endl;
+   else if(num >= 60)
+   {cout<<"grade D."<<endl;
+    else if(num >= 50)
+    {cout<<"grade E."<<endl;
+else
+{cout<<"grade will be F."<<endl;
+    return 0;
+}
